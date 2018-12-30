7e9a557fd40cecc36f9e1363e43edd5126238894
Kyle Gray <kgray1497@gmail.com>
2018-12-29 22:29:02 -0500

    Adding more

--- src/areas/App/index.js
+++ src/areas/App/index.js
@@ -1,15 +1,15 @@
 import React from "react";
+import ProfilePic from 'assets/ProfilePic.jpg';
+import 'animate.css/animate.min.css';
 import "./style.scss";
 
 export default () => (
-  <div className="app-container">
-    <div className="">
-      <h4 className="">
-        I am currently making changes to this site. It will be back up soon!
-      </h4>
-      <span className="gear-icon icon">
-        <i className="fas fa-cog fa-3x" />
-      </span>
-    </div>
+  <div className="app-container animated bounceIn delay-2s slow">
+    <figure className="image is-128x128">
+      <img
+        className="is-rounded"
+        src={ProfilePic}
+      />
+    </figure>
   </div>
 );
